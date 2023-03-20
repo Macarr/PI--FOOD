76480d8003d1e22900d0c6583bfb3a5df6a61243
Macarr <rodriguezruccimacarena@gmail.com>
2023-03-19 23:22:08 -0300

    Cambios en el form y el paginado

--- api/src/middlewares/validate.js
+++ api/src/middlewares/validate.js
@@ -1,11 +1,12 @@
 const validate = (req, res, next) => {
-  const { name, image, summary, healthScore, steps } = req.body;
+  const { name, image, summary, healthScore, steps, Diets } = req.body;
   if (!name) return res.status(400).json({ error: "missing name" });
   if (!image) return res.status(400).json({ error: "missing image" });
   if (!summary) return res.status(400).json({ error: "missing summary" });
   if (!healthScore)
     return res.status(400).json({ error: "missing healthsocre" });
   if (!steps) return res.status(400).json({ error: "missing steps" });
+  if (Diets.length < 1) return res.status(400).json({ error: "missing diets" });
   next();
   //pasarla a una carpeta que se llame middelwares
 };
